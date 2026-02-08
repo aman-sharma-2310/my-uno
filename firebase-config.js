2dad93f492698987e7dc43d8d2c9865d71e1cde0
Aman Sharma <145845470+aman-sharma-2310@users.noreply.github.com>
2026-02-08 18:36:16 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getDatabase, onValue, ref, set }  from "https://www.gstatic.com/firebasejs/11.0.0/firebase-database.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyB_ADQx7ahelrhnQXrB0XLjeG7KufVqFvo",
-    authDomain: "my-uno-f033e.firebaseapp.com",
-    databaseURL: "https://my-uno-f033e-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "my-uno-f033e",
-    storageBucket: "my-uno-f033e.firebasestorage.app",
-    messagingSenderId: "461966569116",
-    appId: "1:461966569116:web:d616e5f9b045184a64c759"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
+
+// 🔥 Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
